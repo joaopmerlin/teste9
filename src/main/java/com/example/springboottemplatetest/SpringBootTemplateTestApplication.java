2263b98c7b4e3fddbc1bdfe13587c0564dd5b2f5
João <joao.p.merlin@gmail.com>
2023-05-29 23:19:32 -0300

    template

---
+++ src/main/java/com/example/springboottemplatetest/SpringBootTemplateTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottemplatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTemplateTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTemplateTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottemplatetest/SpringBootTemplateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottemplatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
